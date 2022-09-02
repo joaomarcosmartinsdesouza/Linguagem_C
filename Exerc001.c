537d00a93a95c2eb407de9e39508978f1cf9c9ae
João Marcos Martins de Souza <84867094+joaomarcosmartinsdesouza@users.noreply.github.com>
2022-09-02 15:20:43 -0300

    Add files via upload

---
+++ Exerc001.c
@@ -0,0 +1,7 @@
+# include <stdio.h>
+
+int main (void){
+    printf("  Primeiro programa em C, testando IDE CODEBLOCKS e GitHub.\n ");
+    printf("   \n   --- Fim do programa. --- \n");
+    return 0;
+}
